e704b625d0ae3aeb2c2a9c13f75b08d174448598
mohdhanif1234 <hanif.topia@gmail.com>
2021-10-11 18:21:10 +0530

    [Hanif]:Added a program to create a simple stack

---
+++ DataStructuresStacksNQueues/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresStacksNQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine("Welcome to  Stack and Queue");
+            Console.WriteLine("****************************************************************");
+            //Object of class LinkedListStack
+            StackWithLinkedList stack = new StackWithLinkedList();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            Console.WriteLine("After pushing some node into stack");
+            stack.DisplayLL();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DataStructuresStacksNQueues/StackWithLinkedList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresStacksNQueues
+{
+    class StackWithLinkedList
+    {   
+        //Initialize the head of the LL
+        internal NodeCreation top;
+        /// <summary>
+        /// Push a new node into stack i.e. add a node in front of LL
+        /// </summary>
+        /// <param name="value"></param>
+        public void Push(int value)
+        {
+            //Create a new Node of LL and add to LL
+            NodeCreation stackNode = new NodeCreation(value);
+            if (top == null)
+            {
+                this.top = stackNode;
+            }
+            else
+            {
+                stackNode.next = this.top;
+                top = stackNode;
+            }
+        }
+        /// <summary>
+        /// display the all the data in Current stack
+        /// </summary>
+        public void DisplayLL()
+        {
+            NodeCreation temp = this.top;
+            if (temp == null)
+            {
+                Console.WriteLine("Given stack is empty");
+            }
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine("Display all the value of stack");
+            Console.WriteLine("****************************************************************");
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
